39c0205d342f0f56676b4d07178ff971b31d6dea
Md. Jaid Bin Hashem <jaid.hashem@bs-23.net>
2021-03-09 13:11:18 +0600

    Add persistor

--- App.js
+++ App.js
@@ -2,28 +2,39 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
+import {getPersistor} from '@rematch/persist';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 
 import PageOneScreen from './app/screens/PageOneScreen';
 import PageTwoScreen from './app/screens/PageTwoScreen';
 import DataScreen from './app/screens/DataScreen';
 
 import store from './store';
-import {SafeAreaView} from 'react-native';
+import {ActivityIndicator, SafeAreaView, View} from 'react-native';
 
+const persistor = getPersistor();
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaView style={{flex: 1}}>
-        <NavigationContainer>
-          <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Page 1" component={PageOneScreen} />
-            <Drawer.Screen name="Page 2" component={PageTwoScreen} />
-            <Drawer.Screen name="Data Page" component={DataScreen} />
-          </Drawer.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+      <PersistGate
+        loading={
+          <View style={{flex: 1, justifyContent: 'center'}}>
+            <ActivityIndicator size="large" color="#2196F3" />
+          </View>
+        }
+        persistor={persistor}>
+        <SafeAreaView style={{flex: 1}}>
+          <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+              <Drawer.Screen name="Page 1" component={PageOneScreen} />
+              <Drawer.Screen name="Page 2" component={PageTwoScreen} />
+              <Drawer.Screen name="Data Page" component={DataScreen} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      </PersistGate>
     </Provider>
   );
 }
